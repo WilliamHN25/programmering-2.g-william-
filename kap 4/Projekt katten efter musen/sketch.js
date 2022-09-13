c492c74a6c166f73736ac414c0cffb9878f5abdd
WilliamHN25 <williamnikolajsen@gmail.com>
2022-09-13 09:26:47 +0200

    ændringer 2

---
+++ kap 4/Projekt katten efter musen/sketch.js
@@ -0,0 +1,17 @@
+let x=random(0,windowWidth)
+let y=random(0,windowHeight)
+let a=speed
+let b=0
+let d=20
+let speed=10
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  speed;
+  circle(x,y,d)
+}
--- kap 4/ex 1/sketch.js
+++ kap 4/ex 1/sketch.js
@@ -1,15 +1,17 @@
-function setup() {
-  createCanvas(400, 400);
-  background(220);
-  circle(200,200,50)
-}
-
-function draw() {
-  background(220);
-  if(mouseX <= 199){
-    circle(mouseX,mouseY,50)
-  }
-  if(mouseX >= 200){
-    rect(mouseX,mouseY,40,50)
-  }
-}
+// =
+console.log(2 == 2); 
+console.log(2 == '2'); 
+/*her bliver begge true*/
+// !=
+console.log(3 != 2); 
+console.log('hello' != 'Hello'); 
+/*her bliver begger true igen, fordi 3 ikker giver 2 og hello med lille ikke er lig Hello med stort*/
+//===
+console.log(2 === 2); 
+console.log(2 === '2'); 
+/*der bliver den første true og den anden false, fordi den første er samem type og tal, hvorimod den anden ikke er
+sammen type*/
+// !==
+console.log(2 !== '2'); 
+console.log(2 !== 2);
+/*her bliver den første true og den anden false, fordi de skal ikke være sammen type for at være true*/

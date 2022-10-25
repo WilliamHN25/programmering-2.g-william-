321a4567ca25aec71f7fad8f1cc6d5e75331775e
WilliamHN25 <williamnikolajsen@gmail.com>
2022-10-25 09:27:20 +0200

    idk

--- kap 5/ex4/sketch.js
+++ kap 5/ex4/sketch.js
@@ -1,7 +1,15 @@
+let i = 0;
+
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
+  fill(255, 0, 0);
+  while (i < 100) {
+    i += 1;
+    if (!(i % 2 == 0)) {
+      fill(0, 0, 255);
+      x = random(400);
+      y = random(400);
+      text(i, x, y);
+    }
+  }
 }
--- kap 5/ex6/sketch.js
+++ kap 5/ex6/sketch.js
@@ -1,7 +1,11 @@
+let r;
+let num = 30;
 function setup() {
   createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
+  fill(255);
+  r = 20;
+  for (i = 0; 1 < num; i++) {
+    circle(200, 200, r);
+    r += 10;
+  }
 }
